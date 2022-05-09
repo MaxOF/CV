ce9b2b05b160e18d142734571041ea446473175c
MaxOF <egorovteam2019@yandex.ru>
2022-05-09 16:55:10 +0300

    refactor css styles for skills

--- src/components/skills/Skills.tsx
+++ src/components/skills/Skills.tsx
@@ -6,6 +6,13 @@ import {Fade} from "react-awesome-reveal";
 import tsLogo from "../../assets/image/skills/Typescript.jpg";
 import reactLogo from "../../assets/image/skills/React.jpg";
 import reduxLogo from "../../assets/image/skills/Redux.jpg";
+import axiosLogo from "../../assets/image/skills/axios.jpg";
+import jestLogo from "../../assets/image/skills/jest.jpg";
+import postmanLogo from "../../assets/image/skills/postman.jpg";
+import muiLogo from "../../assets/image/skills/MUI.jpg";
+import formikLogo from "../../assets/image/skills/formik.jpg";
+import scssLogo from "../../assets/image/skills/scss.jpg";
+
 
 const Skills = () => {
 
@@ -17,6 +24,24 @@ const Skills = () => {
     }
     const redux = {
         backgroundImage: `url(${reduxLogo})`
+    }
+    const axios = {
+        backgroundImage: `url(${axiosLogo})`
+    }
+    const jest = {
+        backgroundImage: `url(${jestLogo})`
+    }
+    const postman = {
+        backgroundImage: `url(${postmanLogo})`
+    }
+    const mui = {
+        backgroundImage: `url(${muiLogo})`
+    }
+    const formik = {
+        backgroundImage: `url(${formikLogo})`
+    }
+    const scss = {
+        backgroundImage: `url(${scssLogo})`
     }
 
 
@@ -30,6 +55,12 @@ const Skills = () => {
                     <Skill title={'TypeScript'} style={ts}/>
                     <Skill title={'React'} style={react}/>
                     <Skill title={'Redux'} style={redux}/>
+                    <Skill title={'Jest/TDD'} style={jest}/>
+                    <Skill title={'Axios'} style={axios}/>
+                    <Skill title={'Postman'} style={postman}/>
+                    <Skill title={'Formik'} style={formik}/>
+                    <Skill title={'Material UI'} style={mui}/>
+                    <Skill title={'SCSS'} style={scss}/>
                 </div>
             </div>
             </Fade>
